4e72a09f86946eb26e1236fd28bf908ba0e2ebaf
LeonovIlya <zlayapisya@gmail.com>
2022-05-04 22:05:17 +0300

    Second version

--- articles/admin.py
+++ articles/admin.py
@@ -1,41 +1,31 @@
 from django.contrib import admin
 from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
-from .models import Article, ArticleTags
+from .models import Article, Relationship, Tag
 
 
-class ArticleTagsInlineFormset(BaseInlineFormSet):
+class RelationshipInlineFormset(BaseInlineFormSet):
     def clean(self):
-        # counter = 0
-        # data_list = list()
-        # if counter < 1:
-        #     for form in self.forms:
-        #         data = form.cleaned_data
-        #         data_list.append(data)
-        #     for data in data_list:
-        #         if 'is_main' in data.keys():
-        #             if data['is_main']:
-        #                 counter += 1
-        # else:
-        #     raise ValidationError('Основным может быть только один раздел')
-
         counter = 0
-        if counter < 1:
-            for form in self.forms:
-                data = form.cleaned_data
-                if data.get('is_main'):
+        for form in self.forms:
+            for key, value in form.cleaned_data.items():
+                if key == 'is_main' and value is True:
                     counter += 1
-        else:
-            raise ValidationError('Основным может быть только один раздел')
+        if counter > 1:
+            raise ValidationError('Основной раздел может быть только один.')
 
         return super().clean()
 
-
-class ArticleTagsInline(admin.TabularInline):
-    model = ArticleTags
-    formset = ArticleTagsInlineFormset
+class RelationshipInline(admin.TabularInline):
+    model = Relationship
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    inlines = [ArticleTagsInline]
+    inlines = [RelationshipInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- website/settings.py
+++ website/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_m2m',
+        'NAME': 'netologym2m',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
